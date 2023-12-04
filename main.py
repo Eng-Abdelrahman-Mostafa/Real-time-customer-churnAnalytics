ba9faf9dbdc6b49d6f5e2894602fd49f6f454a1c
Eng-Abdelrahman-Mostafa <abdelrahmanmostafa785@gmail.com>
2023-12-05 00:37:31 +0200

    fix mysql error

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ spark = SparkSession.builder\
 kafka_df = spark.readStream.format("kafka")\
     .option("kafka.bootstrap.servers", "localhost:9092")\
     .option("subscribe", "customer-info-topic")\
-    .option("startingOffsets", "latest")\
+    .option("startingOffsets", "earliest")\
     .load()
 
 # Define the schema for the JSON data
